87a65dd7f580e76218168ea31b02083e19d65962
Adit <aditsharma1203@gmail.com>
2024-07-06 00:25:49 +0530

    added presentation and report pdfs, also removed stop words and performed stemming in the pre processing stage

--- app.py
+++ app.py
@@ -1,28 +1,59 @@
 import streamlit as st
+import re
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Load the Random Forest model from the file
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+
+
+
+# Load the Random Forest model
 with open('random_forest_model.pkl', 'rb') as file:
     loaded_rf_model = pickle.load(file)
 
-# Load the TfidfVectorizer from the file
+# Load the TfidfVectorizer
 with open('tfidf_vectorizer.pkl', 'rb') as file:
     tfidf_vectorizer = pickle.load(file)
 
-# Function to preprocess input
-def preprocess_input(email_text):
-    email_vectorized = tfidf_vectorizer.transform([email_text])
-    return email_vectorized
+# preprocess input
+def preprocess_text(text):
+    # Ensure the text is treated as a string
+    text = str(text)
+    # Convert to lowercase
+    text = text.lower()
+    # Remove special characters except for common ones like @ and . in email addresses
+    text = re.sub(r'[^a-zA-Z0-9@.\s]', ' ', text)
+    # Replace multiple spaces with a single space
+    text = re.sub(r'\s+', ' ', text).strip()
 
-# Streamlit UI
+
+
+
+
+    words = word_tokenize(text)
+
+    filtered_sentence = [stemmer.stem(word) for word in words if word not in stop_words]
+
+
+    return ' '.join(filtered_sentence)
+
+
+
+
 st.title("Phishing Email Detection")
 
 email_text = st.text_area("Enter the email text:")
 
 if st.button("Predict"):
-    preprocessed_email = preprocess_input(email_text)
-    prediction = loaded_rf_model.predict(preprocessed_email)
+    preprocessed_email = preprocess_text(email_text)
+    email_vectorized = tfidf_vectorizer.transform([preprocessed_email])
+    prediction = loaded_rf_model.predict(email_vectorized)
     
     if prediction[0] == "Phishing Email":
         st.error("The email is classified as a phishing email.")
